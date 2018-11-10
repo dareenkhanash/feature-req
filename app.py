30250ffcc7843bbdf0ce6cf01421e423088c6eec
dareenkhanash <dareenhanash1988@gmail.com>
2018-11-10 17:15:22 +0200

    adding add  request form

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ def features():
 
 
 @app.route('/request')
-def request():
-    return render_template('add-request.html')
+@app.route('/request/<request_id>')
+def editrRequest(request_id=None):
+    return render_template('request.html',request_id=request_id)
 
 
 if __name__ == '__main__':

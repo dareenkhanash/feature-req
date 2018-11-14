e751cbd3b713af6990201a5ad0dae6223c1b88ce
dareenkhanash <dareenhanash1988@gmail.com>
2018-11-15 00:55:28 +0200

    adds database URI

--- feature_req/__init__.py
+++ feature_req/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+
 app = Flask(__name__)
+os.environ['DATABASE_URL'] = <URL>
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+print(os.environ['DATABASE_URL'])
 db = SQLAlchemy(app)
 
 

6e67f8e637b34a5c02d1adca6cdb7d4918b99f27
dareenkhanash <dareenhanash1988@gmail.com>
2018-11-15 00:33:26 +0200

    adds database URI

--- feature_req/__init__.py
+++ feature_req/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+ import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =  "postgres://kkfyldiafopfgc:fec67dbae9bcfe5d68ed60f4389959c09e64c3b9ebfece493f512caa60340b9c@ec2-54-204-10-231.compute-1.amazonaws.com:5432/dfvsp60t634an1"
+app.config['SQLALCHEMY_DATABASE_URI'] =   os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 

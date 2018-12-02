9b03dc6dbc9e493b2329f922bbb98dee1807a83d
dareenkhanash <dareenhanash1988@gmail.com>
2018-12-02 22:36:59 +0200

    delete comments

--- feature_req/app.py
+++ feature_req/app.py
@@ -8,12 +8,12 @@ db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config) 
+    app.config.from_object(config_class) 
     db.init_app(app)
-    
+
     from .routes import feature_req
     app.register_blueprint(feature_req)
-
+    
     return app
    
 
@@ -22,23 +22,4 @@ def create_app(config_class=Config):
 
 
 
-####
-#from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-#import os
 
-
-#app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#db = SQLAlchemy(app)
-
-
-#from feature_req import routes
-
-#app.register_blueprint(routes)
-
-#import os
-
-#if __name__ == '__main__':
-   # port = int(os.environ.get('PORT', 5000))
-  #  app.run(host= '0.0.0.0',port=port)
